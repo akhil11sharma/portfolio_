85d62d4bdac750f0f09c642597d488ac54b3f363
Akhil Sharma <142882022+akhil11sharma@users.noreply.github.com>
2024-07-08 20:12:53 +0530

    educations.js

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -1,19 +1,19 @@
 export const educations = [
   {
     id: 1,
-    title: "Bachelor Degree",
+    title: "Bachelor of Technology in Computer Science & Engineering",
     duration: "2022-Present",
     institution: "Sharda University,Gautam Buddha Nagar",
   },
   {
     id: 2,
-    title: "Higher Secondary Certificate",
+    title: "Higher Secondary, CBSE",
     duration: "2020 - 2022",
     institution: "St. Mary's Christian School, Ghaziabad",
   },
   {
     id: 3,
-    title: "Secondary School Certificate",
+    title: "Secondary School, CBSE",
     duration: "2018 - 2020",
     institution: "St. Mary's Christian School, Ghaziabad",
   }
