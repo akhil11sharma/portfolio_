8fe997cc28be6779a5af3a1a7460afdc32faf24a
Akhil Sharma <142882022+akhil11sharma@users.noreply.github.com>
2024-07-08 20:34:43 +0530

    projects-data.js

--- utils/data/projects-data.js
+++ utils/data/projects-data.js
@@ -6,43 +6,43 @@ import travel from '/public/image/travel.jpg';
 export const projectsData = [
     {
         id: 1,
-        name: 'AI Powered Financial App',
-        description: "Me and my team built an AI-powered financial mobile application. I have developed API using Express, Typescript, OpenAI, AWS, and MongoDB. Used OTP via AWS SES, Google, and Facebook for the authentication system. Built AI assistants using OpenAI's latest model and trained using our dataset. Voice messages are converted to text using AWS Transcribe. The app fetches data from Google Sheets and generates a PDF term sheet, sent via AWS SES.",
-        tools: ['Express', 'MongoDB', 'OpenAI API', 'AWS SES', 'AWS S3', 'Node Mailer', 'Joi', 'Puppeteer', 'EC2', 'PM2', 'Nginx'],
-        role: 'Backend Developer',
+        name: 'Database Design for Teree Armee',
+        description: "I have designed a database for an organization named Teree Armee to store datasets and avoid overwriting and over-replacing issues. Using Python, MySQL, and SSMS, I ensured data storage as well as immutability by storing formulas, table structures, and row/column counts in a separate table. Additionally, I worked on two databases simultaneously.",
+        tools: ['Python', 'MySQL', 'SSMS'],
+        role: 'Database Design Intern',
         code: '',
         demo: '',
-        image: crefin,
+        image: 'crefin', // Assuming 'crefin' is the correct image reference
     },
     {
         id: 2,
-        name: 'Travel Agency App',
-        description: 'I have designed and developed a full-stack web app for 2Expedition, a travel agency in Armenia. I created the UI using NextJS, Typescript, MUI, TailwindCSS, Google Maps, Sun-Editor, and React Slick. The app supports multiple languages and currencies. I developed the API using NestJS, Typescript, MySQL, TypeORM, AWS, and Nodemailer. I deployed the front-end app to AWS Amplify and the back-end app to AWS EC2.',
-        tools: ['NextJS', 'Tailwind CSS', "Google Maps", "NestJS", "TypeScript", "MySQL", "AWS S3", "Sun-Editor", "Gmail Passkey"],
-        role: 'Full Stack Developer',
+        name: 'Explore Cinematic Excellence',
+        description: "This website, hosted locally, allows users to search for movies and display comprehensive details, including watch hours and awards. Developed using HTML, CSS, and JavaScript.",
+        tools: ['HTML', 'CSS', 'JavaScript'],
+        role: 'Personal Project',
         code: '',
         demo: '',
-        image: travel,
+        image: 'travel', // Assuming 'travel' is the correct image reference
     },
     {
         id: 3,
-        name: 'AI Powered Real Estate',
-        description: 'My team built an AI-based real estate app using Replicate API and OpenAI. We used Express, Typescript, OpenAI, Replicate, Stripe, and Mongoose to develop the API. We utilized NextJS, Formik, TailwindCSS, and other npm libraries for the UI. We have trained multiple AI assistants using the latest GPT model and integrated Replicate API for image processing. We added role-based auth, subscription plans, Cron job scheduling, and payment integration with Stripe.',
-        tools: ['React', 'Bootstrap', 'SCSS', 'Stripe', 'Express', 'TypeScript', 'MongoDB', 'Azure Blob', 'OpenAI API', 'Replicate AI', 'Cronjob', 'JWT'],
+        name: 'LinkedIn Limit Exhauster',
+        description: "Using Selenium, I designed a webdriver to add people based on a search table, facilitating random connections and managing LinkedIn time duration.",
+        tools: ['Selenium', 'Python'],
+        role: 'Personal Project',
         code: '',
-        role: 'Full Stack Developer',
         demo: '',
-        image: realEstate,
+        image: 'realEstate', // Assuming 'realEstate' is the correct image reference
     },
     {
         id: 4,
-        name: 'Newsroom Management',
-        description: "My team and I developed a newspaper management dashboard application called Newsroom Management. As a front-end developer, I worked on creating the dashboard using NextJS, Material UI, Redux, Calendar, and other necessary npm libraries. We used React Redux to manage the application's state and React-hook-form and Sun Editor to handle forms.",
-        tools: ['NextJS', 'Material UI', 'Redux', 'Sun Editor', "Calendar"],
-        code: '',
+        name: 'Liver Disease Prediction and Analysis',
+        description: "Associated with Sharda University, this project uses machine learning algorithms to predict liver disease using the Indian Liver Patient Records dataset. Steps included data preprocessing, standardization, feature scaling, and employing various classification algorithms. Neural networks achieved the highest accuracy of 77%, followed by SVM and gradient boosting at 73% and 70%, respectively.",
+        tools: ['Python', 'Machine Learning', 'Neural Networks', 'SVM', 'Gradient Boosting'],
+        role: 'University Project',
+        code: 'https://www.kaggle.com/code/jg7demon/ml-algorithms-for-liver-disease-detection',
         demo: '',
-        image: ayla,
-        role: 'Full Stack Developer',
+        image: 'ayla', // Assuming 'ayla' is the correct image reference
     }
 ];
 
